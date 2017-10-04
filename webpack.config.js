f1de3f7cdeb1406c3fa71c432532f15be3dd4e85
Alex Goldvarg <agoldvarg@gmail.com>
2017-10-04 15:53:19 -0400

    try out one of rule for webpack style loading

--- webpack.config.js
+++ webpack.config.js
@@ -13,11 +13,22 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
-          { loader: 'sass-loader' },
+        oneOf: [
+          {
+            test: /\.css$/,
+            use: [
+              { loader: 'style-loader' },
+              { loader: 'css-loader' },
+            ]
+          },
+          {
+            test: /\.scss$/,
+            use: [
+              { loader: 'style-loader' },
+              { loader: 'css-loader' },
+              { loader: 'sass-loader' },
+            ]
+          },
         ]
       },
       {
