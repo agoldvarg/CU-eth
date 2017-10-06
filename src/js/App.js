7d2f17848873fac5df741c48a628067121affb38
Alex Goldvarg <agoldvarg@gmail.com>
2017-10-06 10:00:40 -0400

    wire up basic ui elements

--- src/js/App.js
+++ src/js/App.js
@@ -1,12 +1,90 @@
 import React, { Component } from 'react';
 
+import CitizensUnited from '../../build/contracts/CitizensUnited.json';
+
 const baseCls = 'App';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      costOfSpeech: null,
+      message: null,
+      tsar: null,
+    };
+
+    const citizensUnited = web3.eth.contract(CitizensUnited.abi);
+    this.contractInstance = citizensUnited.at('0x3aa10d7fd6c6b947d83f2cdd90fb6878d2967430');
+
+    this.fetchContractState = this.fetchContractState.bind(this);
+    this.setMessage = this.setMessage.bind(this);
+
+    setInterval(this.fetchContractState, 5000);
+    // console.log(this.contractInstance);
+  }
+
+  fetchContractState() {
+    this.contractInstance.costOfSpeech((error, result) => {
+      if (!error) {
+        const costOfSpeech = parseFloat(web3.fromWei(result, 'ether'));
+        this.setState({ costOfSpeech });
+      } else {
+        console.log('[error] costOfSpeech:', error)
+      }
+    });
+    this.contractInstance.message((error, message) => {
+      if (!error) {
+        this.setState({ message });
+      } else {
+        console.log('[error] message:', error)
+      }
+    });
+    this.contractInstance.tsar((error, tsar) => {
+      if (!error) {
+        this.setState({ tsar });
+      } else {
+        console.log('[error] tsar:', error)
+      }
+    });
+  }
+
+  setMessage() {
+    const message = this.msgInput.value;
+    const value = parseInt(this.valueInput.value);
+
+    const txOptions = {
+      from: web3.eth.accounts[0],
+      value: web3.toWei(value, 'ether')
+    };
+
+    this.contractInstance.setMessage(message, txOptions, (error, result) => {
+      // console.log(error, result);
+    });
+  }
+
   render() {
     return (
       <div className={baseCls}>
-        Hello
+        <div className="meta">
+          cost of speech: {this.state.costOfSpeech}
+        </div>
+        <div className="meta">
+          tsar: {this.state.tsar}
+        </div>
+        <div className="meta">
+          message: {this.state.message}
+        </div>
+        <div className="form">
+          <label htmlFor="msgInput">message
+          <input name="msgInput" type="text" ref={el => this.msgInput = el} />
+          </label>
+          <label htmlFor="valueInput">value
+          <input name="valueInput" type="text" ref={el => this.valueInput = el} />
+          </label>
+
+          <button onClick={this.setMessage}>SUBMIT</button>
+        </div>
       </div>
     );
   }
--- src/js/index.js
+++ src/js/index.js
@@ -7,15 +7,29 @@ import App from './App';
 
 const setupWeb3 = () => {
   if (typeof window.web3 !== 'undefined') {
-    console.warn('[web3]: using injected web3 instance');
+    console.log('[web3] using injected web3 instance');
 
     window.web3 = new Web3(window.web3.currentProvider);
   } else {
-    console.warn('[web3]: falling back to localhost');
+    console.log('[web3] falling back to localhost');
 
     window.web3 = new Web3(new Web3.providers.HttpProvider("http://locahost:8545"))
   }
 }
 
+const getActiveAddress = () =>
+  window.web3.eth.getAccounts((error, addresses) =>
+    !error && setActiveAddress(addresses[0]));
+
+const setActiveAddress = (addr) =>
+  window.activeAddress = addr;
+
+const accountInterval = setInterval(() => {
+  if (!window.web3) {
+    return;
+  }
+
+  getActiveAddress();
+}, 1000);
 
 ReactDOM.render(<App />, document.getElementById('root'), setupWeb3);
